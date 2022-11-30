0c911d1073f8fa8e40bfde27c81283f69233273d
d-joedel <joedeldacudao27@gmail.com>
2022-11-30 17:57:00 +0800

    add bin, lib, src..

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args)  {
+         
+    for (int j=0; j<5 ; j++ )
+    {
+        System.out.print("*"); 
+
+        }
+    
+
+        
+  
+
+
+    for (int i=0; i<5 ; i++)
+    {
+        System.out.println("*");
+
+                  }
+           }
+   }
